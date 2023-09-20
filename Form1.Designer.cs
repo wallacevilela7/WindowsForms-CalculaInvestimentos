6049c6ba64cce028983aca12e9ee1f315e3154a6
Wallace Vilela <wallace.vilela9@gmail.com>
2023-09-20 18:18:27 -0300

    Design e btn Calcular

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,18 +28,133 @@
         /// </summary>
         private void InitializeComponent()
         {
+            btnCalcular = new Button();
+            txtPeriodo = new TextBox();
+            txtTaxaJuros = new TextBox();
+            txtValorAplicado = new TextBox();
+            txtResult = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label5 = new Label();
+            btnLimpar = new Button();
             SuspendLayout();
+            // 
+            // btnCalcular
+            // 
+            btnCalcular.Location = new Point(68, 236);
+            btnCalcular.Name = "btnCalcular";
+            btnCalcular.Size = new Size(183, 32);
+            btnCalcular.TabIndex = 0;
+            btnCalcular.Text = "Calcular";
+            btnCalcular.UseVisualStyleBackColor = true;
+            btnCalcular.Click += button1_Click;
+            // 
+            // txtPeriodo
+            // 
+            txtPeriodo.Location = new Point(68, 62);
+            txtPeriodo.Name = "txtPeriodo";
+            txtPeriodo.Size = new Size(183, 23);
+            txtPeriodo.TabIndex = 1;
+            // 
+            // txtTaxaJuros
+            // 
+            txtTaxaJuros.Location = new Point(68, 128);
+            txtTaxaJuros.Name = "txtTaxaJuros";
+            txtTaxaJuros.Size = new Size(183, 23);
+            txtTaxaJuros.TabIndex = 2;
+            // 
+            // txtValorAplicado
+            // 
+            txtValorAplicado.Location = new Point(68, 190);
+            txtValorAplicado.Name = "txtValorAplicado";
+            txtValorAplicado.Size = new Size(183, 23);
+            txtValorAplicado.TabIndex = 3;
+            // 
+            // txtResult
+            // 
+            txtResult.Location = new Point(68, 379);
+            txtResult.Name = "txtResult";
+            txtResult.Size = new Size(183, 23);
+            txtResult.TabIndex = 5;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(68, 44);
+            label1.Name = "label1";
+            label1.Size = new Size(48, 15);
+            label1.TabIndex = 6;
+            label1.Text = "Período";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(68, 110);
+            label2.Name = "label2";
+            label2.Size = new Size(80, 15);
+            label2.TabIndex = 7;
+            label2.Text = "Juros Mensais";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(68, 172);
+            label3.Name = "label3";
+            label3.Size = new Size(83, 15);
+            label3.TabIndex = 8;
+            label3.Text = "Valor Aplicado";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(68, 361);
+            label5.Name = "label5";
+            label5.Size = new Size(47, 15);
+            label5.TabIndex = 10;
+            label5.Text = "Ganhos";
+            // 
+            // btnLimpar
+            // 
+            btnLimpar.Location = new Point(68, 292);
+            btnLimpar.Name = "btnLimpar";
+            btnLimpar.Size = new Size(181, 31);
+            btnLimpar.TabIndex = 11;
+            btnLimpar.Text = "Limpar";
+            btnLimpar.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(448, 547);
+            Controls.Add(btnLimpar);
+            Controls.Add(label5);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(txtResult);
+            Controls.Add(txtValorAplicado);
+            Controls.Add(txtTaxaJuros);
+            Controls.Add(txtPeriodo);
+            Controls.Add(btnCalcular);
             Name = "Form1";
             Text = "Form1";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button btnCalcular;
+        private TextBox txtPeriodo;
+        private TextBox txtTaxaJuros;
+        private TextBox txtValorAplicado;
+        private TextBox txtResult;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label5;
+        private Button btnLimpar;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -2,9 +2,37 @@ namespace CalculaJuros
 {
     public partial class Form1 : Form
     {
+        private int periodoAnos = 0, periodoMeses = 0;
+        private double taxaJuros = 0.0, valorAplicado = 0.0, result = 0.0;
+
+
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                periodoAnos = int.Parse(txtPeriodo.Text);
+                taxaJuros = double.Parse(txtTaxaJuros.Text);
+                valorAplicado = double.Parse(txtValorAplicado.Text);
+
+                periodoMeses = periodoAnos * 12;
+
+                double valorBase = 1 + taxaJuros;
+                double valorExpoente = Math.Pow(1 + taxaJuros, periodoMeses);
+
+                result = valorAplicado * valorExpoente;
+                txtResult.Text = $"{result}";
+            }
+            catch (Exception ex)
+            {
+                txtResult.Text = ex.Message;    
+            }
         }
     }
 }
